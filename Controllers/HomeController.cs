1ea0c6630263842d14dc350840bc9ac8071e369f
Jalal Hejazi <jalal.hejazi@gmail.com>
2012-09-05 14:10:37 +0200

    jQueryMobile MVC4
    Using Visual Studio 2012 MVC4 jQueryMobile Template

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JalalMobile.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "jQuerymobile MVC and REST DataServices";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This code is developed using Visual Studio 2012";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Jalal.Hejazi@gmail.com";
+
+            return View();
+        }
+    }
+}
